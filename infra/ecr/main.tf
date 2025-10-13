d1fdc4a3eaa3312f4c429c45fb79c52c4eb3631e
akshu78602 <akshu78602@gmail.com>
2025-10-12 19:58:08 -0700

    fix syntax

--- infra/ecr/main.tf
+++ infra/ecr/main.tf
@@ -1,7 +1,7 @@
 terraform{
     required_version= ">1.13.3"
     required_providers{
-        aws= {source= "hashicorp/aws, version = ">=5.0, <6.0}
+        aws= {source= "hashicorp/aws", version = ">=5.0, <6.0"}
     }
 
 }
